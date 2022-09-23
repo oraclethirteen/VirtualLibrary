305f20c26f9a3b1a5795c3febee471cee77652e5
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-09-23 19:55:19 +0300

    Add project files.

---
+++ VirtualLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
